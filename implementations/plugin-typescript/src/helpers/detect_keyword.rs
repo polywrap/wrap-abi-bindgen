48537b3dd9801af3577c19bf06bb764ed9e5a7ac
krisbitney <kristoferbitney@hotmail.com>
2023-06-14 16:08:15 -0500

    added detect_keyword, is_keyword, and to_typescript to typescript plugin bindings

---
+++ implementations/plugin-typescript/src/helpers/detect_keyword.rs
@@ -0,0 +1,15 @@
+use handlebars::handlebars_helper;
+use serde_json::{Value};
+use crate::helpers::is_keyword::_is_keyword;
+
+handlebars_helper!(detect_keyword: |val: Value| -> String {
+    let type_val = val.as_str().unwrap();
+    _detect_keyword(type_val)
+});
+
+pub fn _detect_keyword(type_val: &str) -> String {
+    if _is_keyword(type_val) {
+        return format!("_{}", type_val);
+    }
+    type_val.to_string()
+}
--- implementations/plugin-typescript/src/helpers/mod.rs
+++ implementations/plugin-typescript/src/helpers/mod.rs
@@ -1,25 +1,15 @@
 use handlebars::Handlebars;
 
-mod apply_optional;
 mod array_has_length;
 mod array_length;
 mod detect_keyword;
-mod is_base_type;
 mod is_keyword;
 mod is_not_first;
 mod is_not_last;
 mod to_graphql_type;
-mod to_msgpack;
-mod to_wasm_array;
-mod to_wasm_init;
-mod to_wasm_map;
-mod to_wasm;
+mod to_typescript;
 
 pub fn register(handlebars: &mut Handlebars) -> () {
-    handlebars.register_helper(
-        "apply_optional",
-        Box::new(apply_optional::apply_optional)
-    );
     handlebars.register_helper(
         "array_has_length",
         Box::new(array_has_length::array_has_length)
@@ -31,10 +21,6 @@ pub fn register(handlebars: &mut Handlebars) -> () {
     handlebars.register_helper(
         "detect_keyword",
         Box::new(detect_keyword::detect_keyword)
-    );
-    handlebars.register_helper(
-        "is_base_type",
-        Box::new(is_base_type::is_base_type)
     );
     handlebars.register_helper(
         "is_keyword",
@@ -53,23 +39,7 @@ pub fn register(handlebars: &mut Handlebars) -> () {
         Box::new(to_graphql_type::to_graphql_type)
     );
     handlebars.register_helper(
-        "to_msgpack",
-        Box::new(to_msgpack::to_msgpack)
-    );
-    handlebars.register_helper(
-        "to_wasm_array",
-        Box::new(to_wasm_array::to_wasm_array)
-    );
-    handlebars.register_helper(
-        "to_wasm_init",
-        Box::new(to_wasm_init::to_wasm_init)
-    );
-    handlebars.register_helper(
-        "to_wasm_map",
-        Box::new(to_wasm_map::to_wasm_map)
-    );
-    handlebars.register_helper(
-        "to_wasm",
-        Box::new(to_wasm::to_wasm)
+        "to_typescript",
+        Box::new(to_typescript::to_typescript)
     );
 }
