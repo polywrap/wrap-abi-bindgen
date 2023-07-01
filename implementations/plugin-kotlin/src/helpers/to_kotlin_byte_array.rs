35c8718828c3976d8655bcecb61c0b5c7bd19a0b
krisbitney <kristoferbitney@hotmail.com>
2023-06-30 21:40:06 -0500

    updated kotlin plugin bindings to use new rust polywrap msgpack serde package

--- implementations/plugin-kotlin/src/helpers/to_kotlin_byte_array.rs
+++ implementations/plugin-kotlin/src/helpers/to_kotlin_byte_array.rs
@@ -1,21 +1,23 @@
 use handlebars::handlebars_helper;
 use serde_json::{Value};
-use polywrap_msgpack::serialize;
+use polywrap_msgpack_serde::{to_vec};
 
 handlebars_helper!(to_kotlin_byte_array: |abi: Value| {
     _to_kotlin_byte_array(abi)
 });
 
 pub fn _to_kotlin_byte_array(abi: Value) -> String {
-    let bytes: Value = serialize(&abi).unwrap()
+    let bytes: Value = to_vec(&abi).unwrap()
         .iter()
         .map(|x| format!("{}.toByte()", x))
         .collect::<Vec<String>>()
         .into();
     let bytes_str: String = bytes.to_string()
+        .replace("\"", "")
+        .replace("\\", "")
         .trim_start_matches("[")
         .trim_end_matches("]")
         .trim()
         .to_string();
-    format!("byteArrayOf(\n{}\n)", bytes_str)
+    format!("byteArrayOf(\n        {}\n        )", bytes_str)
 }
