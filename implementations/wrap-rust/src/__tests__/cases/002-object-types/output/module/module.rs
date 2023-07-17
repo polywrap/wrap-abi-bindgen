1dc8937293c47fe9c35c7f2c48000696bea65d58
Cesar <cesarbrazon10@gmail.com>
2023-07-18 00:21:58 +0200

    chore: add module in object type test case

---
+++ implementations/wrap-rust/src/__tests__/cases/002-object-types/output/module/module.rs
@@ -0,0 +1,20 @@
+use polywrap_msgpack_serde::{
+    wrappers::polywrap_json::JSONString,
+    wrappers::polywrap_bigint::BigIntWrapper
+};
+use polywrap_wasm_rs::{
+    BigInt,
+    BigNumber,
+    Map,
+    JSON
+};
+use crate::{
+    ArgsMethod
+};
+use crate::Arg;
+
+pub struct Module;
+
+pub trait ModuleTrait {
+  fn method(args: ArgsMethod) -> Result<Output, String>;
+}
---
+++ implementations/wrap-rust/src/__tests__/cases/002-object-types/output/module/wrapped.rs
@@ -0,0 +1,42 @@
+use serde::{Deserialize, Serialize};
+use polywrap_msgpack_serde::{
+    from_slice,
+    to_vec,
+    wrappers::polywrap_json::JSONString,
+    wrappers::polywrap_bigint::BigIntWrapper
+};
+use polywrap_wasm_rs::{
+    BigInt,
+    BigNumber,
+    Map,
+    JSON,
+    wrap_load_env
+};
+use crate::module::{ModuleTrait, Module};
+use crate::Arg;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ArgsMethod {
+    pub arg: Arg
+}
+
+pub fn method_wrapped(args: &[u8], env_size: u32) -> Vec<u8> {
+    match from_slice::<ArgsMethod>(args) {
+        Ok(args) => {
+            let result = Module::method1(ArgsMethod {
+                arg: args.arg
+            });
+            match result {
+                Ok(res) => {
+                    to_vec(&res).unwrap()
+                }
+                Err(e) => {
+                    panic!("{}", e.to_string())
+                }
+            }
+        }
+        Err(e) => {
+            panic!("{}", e.to_string())
+        }
+    }
+}
