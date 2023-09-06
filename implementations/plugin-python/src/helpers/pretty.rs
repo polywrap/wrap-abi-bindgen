58241646395e7db64b4c253a32c68c7ebf29b852
Niraj Kamdar <nirajkamdar27@pm.me>
2023-09-06 17:26:24 +0530

    debug: issue

--- implementations/plugin-python/src/helpers/pretty.rs
+++ implementations/plugin-python/src/helpers/pretty.rs
@@ -3,7 +3,7 @@ use serde_json::{Value};
 
 handlebars_helper!(pretty: |value: Value| {
     let mut json_str = serde_json::to_string(&value).unwrap();
-    json_str = json_str.replace("\n", "\\n"); // escape backslashes
+    json_str = json_str.replace("\n", "\\\n"); // escape backslashes
     let value = serde_json::from_str::<Value>(&json_str).unwrap();
     serde_json::to_string_pretty(&value).unwrap()
 });
