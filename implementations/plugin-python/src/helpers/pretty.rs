4300527098f6837cc2abab576231a14d3d205962
Niraj Kamdar <nirajkamdar27@pm.me>
2023-09-06 17:11:58 +0530

    debug: escape newline issue

--- implementations/plugin-python/src/helpers/pretty.rs
+++ implementations/plugin-python/src/helpers/pretty.rs
@@ -2,6 +2,8 @@ use handlebars::handlebars_helper;
 use serde_json::{Value};
 
 handlebars_helper!(pretty: |value: Value| {
-    let str = serde_json::to_string_pretty(&value).unwrap();
-    str.replace("\\", "\\") // escape backslashes
+    let mut json_str = serde_json::to_string(&value).unwrap();
+    json_str = json_str.replace("\n", "\\n"); // escape backslashes
+    let value = serde_json::from_str::<Value>(&json_str).unwrap();
+    serde_json::to_string_pretty(&value).unwrap()
 });
