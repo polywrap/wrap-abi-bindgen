0d816ab88177fada3d464ab742d43be267e06c9b
Niraj Kamdar <nirajkamdar27@pm.me>
2023-09-06 15:50:31 +0530

    fix: wrap.info for polywrap python plugin

--- implementations/plugin-python/src/__tests__/cases/000-sanity/output/wrap_info.py
+++ implementations/plugin-python/src/__tests__/cases/000-sanity/output/wrap_info.py
@@ -1193,6 +1193,7 @@ abi = json.loads("""
             "type": "[AnotherType]"
           }
         ],
+        "comment": "objectMethod docstring \\n is multiline.",
         "env": {
           "required": true
         },
--- implementations/plugin-python/src/helpers/pretty.rs
+++ implementations/plugin-python/src/helpers/pretty.rs
@@ -2,5 +2,6 @@ use handlebars::handlebars_helper;
 use serde_json::{Value};
 
 handlebars_helper!(pretty: |value: Value| {
-    serde_json::to_string_pretty(&value).unwrap()
+    let str = serde_json::to_string_pretty(&value).unwrap();
+    str.replace("\\", "\\") // escape backslashes
 });
